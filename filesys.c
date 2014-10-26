e5e8b3ae0905abd9a56ebbc41d400e3852d14757
Charlie Egan <charlieegan3@googlemail.com>
2014-10-26 13:14:02 +0000

    First Commit

---
+++ filesys.c
@@ -0,0 +1,106 @@
+/* filesys.c
+ *
+ * provides interface to virtual disk
+ *
+ */
+#include "filesys.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+diskblock_t  virtualDisk [MAXBLOCKS] ;           // define our in-memory virtual, with MAXBLOCKS blocks
+fatentry_t   FAT         [MAXBLOCKS] ;           // define a file allocation table with MAXBLOCKS 16-bit entries
+fatentry_t   rootDirIndex            = 0 ;       // rootDir will be set by format
+direntry_t * currentDir              = NULL ;
+fatentry_t   currentDirIndex         = 0 ;
+
+/* writedisk : writes virtual disk out to physical disk
+ *
+ * in: file name of stored virtual disk
+ */
+
+void writedisk ( const char * filename )
+{
+   printf ( "writedisk> virtualdisk[0] = %s\n", virtualDisk[0].data ) ;
+   FILE * dest = fopen( filename, "w" ) ;
+   if ( fwrite ( virtualDisk, sizeof(virtualDisk), 1, dest ) < 0 )
+      fprintf ( stderr, "write virtual disk to disk failed\n" ) ;
+   //write( dest, virtualDisk, sizeof(virtualDisk) ) ;
+   fclose(dest) ;
+
+}
+
+void readdisk ( const char * filename )
+{
+   FILE * dest = fopen( filename, "r" ) ;
+   if ( fread ( virtualDisk, sizeof(virtualDisk), 1, dest ) < 0 )
+      fprintf ( stderr, "read from virtual disk to disk failed\n" ) ;
+   //write( dest, virtualDisk, sizeof(virtualDisk) ) ;
+      fclose(dest) ;
+}
+
+
+/* the basic interface to the virtual disk
+ * this moves memory around
+ */
+
+void writeblock ( diskblock_t * block, int block_address )
+{
+   //printf ( "writeblock> block %d = %s\n", block_address, block->data ) ;
+   memmove ( virtualDisk[block_address].data, block->data, BLOCKSIZE ) ;
+   //printf ( "writeblock> virtualdisk[%d] = %s / %d\n", block_address, virtualDisk[block_address].data, (int)virtualDisk[block_address].data ) ;
+}
+
+
+/* read and write FAT
+ *
+ * please note: a FAT entry is a short, this is a 16-bit word, or 2 bytes
+ *              our blocksize for the virtual disk is 1024, therefore
+ *              we can store 512 FAT entries in one block
+ *
+ *              how many disk blocks do we need to store the complete FAT:
+ *              - our virtual disk has MAXBLOCKS blocks, which is currently 1024
+ *                each block is 1024 bytes long
+ *              - our FAT has MAXBLOCKS entries, which is currently 1024
+ *                each FAT entry is a fatentry_t, which is currently 2 bytes
+ *              - we need (MAXBLOCKS /(BLOCKSIZE / sizeof(fatentry_t))) blocks to store the
+ *                FAT
+ *              - each block can hold (BLOCKSIZE / sizeof(fatentry_t)) fat entries
+ */
+
+/* implement format()
+ */
+void format ( )
+{
+   diskblock_t block ;
+   direntry_t  rootDir ;
+   int         pos             = 0 ;
+   int         fatentry        = 0 ;
+   int         fatblocksneeded =  (MAXBLOCKS / FATENTRYCOUNT ) ;
+
+   /* prepare block 0 : fill it with '\0',
+    * use strcpy() to copy some text to it for test purposes
+	* write block 0 to virtual disk
+	*/
+
+	/* prepare FAT table
+	 * write FAT blocks to virtual disk
+	 */
+
+	 /* prepare root directory
+	  * write root directory block to virtual disk
+	  */
+
+}
+
+
+/* use this for testing
+ */
+
+void printBlock ( int blockIndex )
+{
+   printf ( "virtualdisk[%d] = %s\n", blockIndex, virtualDisk[blockIndex].data ) ;
+}
+
---
+++ filesys.h
@@ -0,0 +1,117 @@
+/* filesys.h
+ *
+ * describes FAT structures
+ * http://www.c-jump.com/CIS24/Slides/FAT/lecture.html#F01_0020_fat
+ * http://www.tavi.co.uk/phobos/fat.html
+ */
+
+#ifndef FILESYS_H
+#define FILESYS_H
+
+#include <time.h>
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+#define MAXBLOCKS     1024
+#define BLOCKSIZE     1024
+#define FATENTRYCOUNT (BLOCKSIZE / sizeof(fatentry_t))
+#define DIRENTRYCOUNT ((BLOCKSIZE - (2*sizeof(int)) ) / sizeof(direntry_t))
+#define MAXNAME       256
+#define MAXPATHLENGTH 1024
+
+#define UNUSED        -1
+#define ENDOFCHAIN     0
+#define EOF           -1
+
+
+typedef unsigned char Byte ;
+
+/* create a type fatentry_t, we set this currently to short (16-bit)
+ */
+typedef short fatentry_t ;
+
+
+// a FAT block is a list of 16-bit entries that form a chain of disk addresses
+
+//const int   fatentrycount = (blocksize / sizeof(fatentry_t)) ;
+
+typedef fatentry_t fatblock_t [ FATENTRYCOUNT ] ;
+
+
+/* create a type direntry_t
+ */
+
+typedef struct direntry {
+   int         entrylength ;   // records length of this entry (can be used with names of variables length)
+   Byte        isdir ;
+   Byte        unused ;
+   time_t      modtime ;
+   int         filelength ;
+   fatentry_t  firstblock ;
+   char   name [MAXNAME] ;
+} direntry_t ;
+
+// a directory block is an array of directory entries
+
+//const int   direntrycount = (blocksize - (2*sizeof(int)) ) / sizeof(direntry_t) ;
+
+typedef struct dirblock {
+   int isdir ;
+   int nextEntry ;
+   direntry_t entrylist [ DIRENTRYCOUNT ] ; // the first two integer are marker and endpos
+} dirblock_t ;
+
+
+
+// a data block holds the actual data of a filelength, it is an array of 8-bit (byte) elements
+
+typedef Byte datablock_t [ BLOCKSIZE ] ;
+
+
+// a diskblock can be either a directory block, a FAT block or actual data
+
+typedef union block {
+   datablock_t data ;
+   dirblock_t  dir  ;
+   fatblock_t  fat  ;
+} diskblock_t ;
+
+// finally, this is the disk: a list of diskblocks
+// the disk is declared as extern, as it is shared in the program
+// it has to be defined in the main program filelength
+
+extern diskblock_t virtualDisk [ MAXBLOCKS ] ;
+
+
+// when a file is opened on this disk, a file handle has to be
+// created in the opening program
+
+typedef struct filedescriptor {
+   int         pos ;           // byte within a block
+   char        mode[3] ;
+   Byte        writing ;
+   fatentry_t  blockno ;
+   diskblock_t buffer ;
+} MyFILE ;
+
+
+
+void format() ;
+void writedisk ( const char * filename ) ;
+
+
+#endif
+
+/*
+#define NUM_TYPES (sizeof types / sizeof types[0])
+static* int types[] = {
+    1,
+    2,
+    3,
+    4 };
+*/
