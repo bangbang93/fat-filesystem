69cf1d292070c5c0430c83341e631c485c718d6c
Charlie Egan <charlieegan3@googlemail.com>
2014-11-19 11:02:50 +0000

    Add changes to shell for different CGS level tasks

--- shell.c
+++ shell.c
@@ -5,19 +5,19 @@
 #include <stdlib.h>
 
 int main() {
+  //CGS D3-D1
   format("CS3026 Operating Systems Assessment 2014\0");
 
-  // save_file();
+  // CGS C3-C1
+  my_file_t *test_file = myfopen("testfile.txt", "a");
+  char string[] = "4096bytes"; //string used for filling the file with data
 
-  my_file_t *test_file = myfopen("testfile.txt", "w");
-
-  for (int i = 0; i < 5; i++){
-    for (int j = 0; j < BLOCKSIZE; j++){
-      // printf("%d\n",j);
-      myfputc('c', test_file);
-    }
+  for (int i = 0; i < (4 * BLOCKSIZE) - 1; i++){
+    // myfputc(string[rand() % (int) (sizeof string - 1)], test_file); //use for random data
+    myfputc(string[1], test_file); //all zeros
   }
 
+  // Save the changes made to the virtual disk
   write_disk("virtualdisk\0");
 
   return 0;
