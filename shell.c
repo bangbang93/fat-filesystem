f4eb0a9f2df73fae6c432aaa270d5e7e2ef9af79
Charlie Egan <charlieegan3@googlemail.com>
2014-11-21 22:27:53 +0000

    Include current shell version

--- shell.c
+++ shell.c
@@ -9,34 +9,42 @@ int main() {
   format("CS3026 Operating Systems Assessment 2014\0");
 
   // CGS C3-C1
-  my_file_t *test_file = myfopen("testfile.txt", "a");
-  char string[] = "4096bytes"; //string used for filling the file with data
+  // my_file_t *test_file = myfopen("testfile.txt", "a");
 
-  print_fat(10);
+  create_file();
 
-  for (int i = 0; i < 4 * BLOCKSIZE; i++){
-    // myfputc(string[rand() % (int) (sizeof string - 1)], test_file); //use for random data
-    myfputc('0', test_file);
-  }
+  // print_block(0, 'd');
+  // print_block(1, 'f');
+  // print_block(2, 'f');
+  print_block(3, 'r');
 
-  myfclose(test_file);
+  // char string[] = "4096bytes"; //string used for filling the file with data
 
-  print_fat(10);
+  // print_fat(10);
 
-  FILE *f = fopen("testfileC3_C1_copy.txt", "w");
-  my_file_t *test_file2 = myfopen("testfile.txt", "r");
-  move_to_data(test_file2);
-  while(1){
-    char character = myfgetc(test_file2);
-    if (character == EOF){
-      break;
-    }
-    printf("%c", character);
-    fprintf(f, "%c", character);
-  }
-  printf("\n");
-  fclose(f);
-  myfclose(test_file2);
+  // for (int i = 0; i < 4 * BLOCKSIZE; i++){
+  //   // myfputc(string[rand() % (int) (sizeof string - 1)], test_file); //use for random data
+  //   myfputc('0', test_file);
+  // }
+
+  // myfclose(test_file);
+
+  // print_fat(10);
+
+  // FILE *f = fopen("testfileC3_C1_copy.txt", "w");
+  // my_file_t *test_file2 = myfopen("testfile.txt", "r");
+  // move_to_data(test_file2);
+  // while(1){
+  //   char character = myfgetc(test_file2);
+  //   if (character == EOF){
+  //     break;
+  //   }
+  //   printf("%c", character);
+  //   fprintf(f, "%c", character);
+  // }
+  // printf("\n");
+  // fclose(f);
+  // myfclose(test_file2);
 
   // Save the changes made to the virtual disk
   write_disk("virtualdisk\0");
