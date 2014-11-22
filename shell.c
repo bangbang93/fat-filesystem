cb7a50eba4a31b7f0b047f5e257add0a82f3697c
Charlie Egan <charlieegan3@googlemail.com>
2014-11-22 23:22:36 +0000

    Current shell.c

--- shell.c
+++ shell.c
@@ -9,14 +9,16 @@ int main() {
   format("CS3026 Operating Systems Assessment 2014\0");
 
   // CGS C3-C1
-  // my_file_t *test_file = myfopen("testfile.txt", "a");
+  my_file_t *test_file = myfopen("test_file.txt", "a");
 
-  create_file();
+  // create_file();
+  print_fat(10);
+  print_directory_structure(3, 0);
 
   // print_block(0, 'd');
   // print_block(1, 'f');
   // print_block(2, 'f');
-  print_block(3, 'r');
+  // print_block(3, 'r');
 
   // char string[] = "4096bytes"; //string used for filling the file with data
 
